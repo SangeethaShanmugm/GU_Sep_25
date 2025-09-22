4f19876a4b232435e80f9a01ab7cc231258a1ae7
SangeethaShanmugm <sanjuriya22@gmail.com>
2025-09-22 16:19:40 +0530

    updated

---
+++ MERN_Overview.md
@@ -0,0 +1,38 @@
+
+24 hours session -> 3 days /week
+4 weeks=> 12 days
+
+Life skills => offline 16 hours
+
+Additionally -> Guvi platform credentials
+
+At end
+------
+
+2 certificate => Employability skill course completion
+                 MERN stack course completion - E Certificate
+                 Trophies - 4 best students
+
+
+MERN
+M -> MongoDB - Database => store application data
+E -> ExpressJs - Framework -> handle server and API
+R -> React - Frontend UI -> Build UI
+N -> NodeJs - Server -> backend server
+
+Full Stack developer role
+--------------------------
+- Front end developer(Client-side)
+- Back end developer(Server-side)
+- Database Developer/Administrator
+- Full stack developer
+
+
+customer => react(UI/menu) => Express (Waiter) => Nodejs(Kitchen) => Mongodb (Pantry)
+                ^
+                |
+               Response/Dish 
+
+
+ let  => your daily lunch order(you can change it tomorrow)  
+ const  => your college ID number            
---
+++ let_const.js
@@ -0,0 +1,88 @@
+// Javascript Es6 basic with example
+
+//let  => variable=> use when the value can change
+
+// let a  = 10;
+// let a => declaration
+// a = 10 => initialization
+//let -  cannot be redeclared but can reinitialize ✅
+// let a = 10;
+// let a = 20;
+// console.log(a)
+
+let b = 10;
+b = 20;
+console.log(b);
+
+
+let lunch = "🍕";
+lunch = "🍔";//can change✅
+
+const collgeID = 12345;
+//collgeID = 12345 // cannot be changed❌
+
+//loop variable
+for (let i = 1; i <= 3; i++) {
+    console.log(i)
+}
+
+// inside the block
+
+let course = "mern";
+
+if (true) {
+    let course = "mean"; //different variable inside block
+    console.log(course)//
+}
+console.log(course)
+
+// const
+const courses = ["maths", "physics", "fullstack"];
+courses.push() //add an element
+console.log(courses) //['maths', 'physics', 'fullstack', 'science']
+
+//regular function => write a long homework
+const functionName = (parameter) => { }
+
+function greet(name) {
+    return "Hello " + name + "👋";
+}
+
+console.log(greet("Ajitha"));
+
+//arrow function => short way of writing function => sending a quick whatsapp message
+
+const greetArrowFunc = (name) => "Hello " + name + "👋";
+
+console.log(greetArrowFunc("Akshaya"));
+
+const sumMarks = (express, nodejs, mongodb, react) => express + nodejs + mongodb + react;
+
+console.log(sumMarks(80, 90, 85, 90))
+
+//array methods
+//map => change each item
+//filter  => pick certain items
+//reduce => combine into one total
+
+const prices = [50, 70, 60];
+
+//map: add tax
+const newPrice = prices.map(p => p + 10);
+console.log(newPrice);
+
+//filter: only > 55
+
+const expensive = prices.filter(p => p > 55);
+console.log(expensive)
+
+//reduce: total price
+//syntax => array.reduce((accumulator, currentValue) => {}, initialValue)
+//accumulator(sum) => stores running result
+//currentValue(p) => current item in array
+//initialValue(0) => starting point
+const total = prices.reduce((sum, p) => sum + p, 0)
+// 1. start with sum = 0(initial value)
+//2. take first number 50 :  0 + 50 =50
+//3 . Next number 70: 50 + 70  = 120
+//4. Next number 60: 120 + 60 = 180
